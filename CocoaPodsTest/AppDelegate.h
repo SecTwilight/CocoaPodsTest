d2d2f2b73b2b1c4cb02ed94db33a957e4624687e
SecTwilight <SecTwilight@gmail.com>
2017-11-18 11:12:06 +0800

    Initial Commit

---
+++ CocoaPodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsTest
+//
+//  Created by SecTwilight on 2017/11/18.
+//  Copyright © 2017年 SecTwilight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaPodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocoaPodsTest
+//
+//  Created by SecTwilight on 2017/11/18.
+//  Copyright © 2017年 SecTwilight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
